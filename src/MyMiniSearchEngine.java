0bad049ffea18d9c9d30c611e36621039942fdf4
ThePurityOfChaos <derickdew9001@gmail.com>
2020-03-05 16:13:51 -0800

    Initial commit

---
+++ src/MyMiniSearchEngine.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class MyMiniSearchEngine {
+    // default solution. OK to change.
+    // do not change the signature of index()
+    private Map<String, List<List<Integer>>> indexes;
+
+    // disable default constructor
+    private MyMiniSearchEngine() {
+        indexes = new HashMap<>();
+        List<Integer> innerList = new ArrayList<>();
+        List<List<Integer>> outerList = new ArrayList<>();
+        outerList.add(innerList);
+        indexes.put("key", outerList);
+    }
+
+    public MyMiniSearchEngine(List<String> documents) {
+        index(documents);
+    }
+
+    // each item in the List is considered a document.
+    // assume documents only contain alphabetical words separated by white spaces.
+
+    private void index(List<String> texts) {
+        List<List<Integer>> depth= new ArrayList<>();
+        for (String text : texts) {
+            String[] words = text.split(" ");
+            indexes.put(Arrays.toString(words), depth);
+        }
+        //homework
+    }
+
+    // search(key) return all the document ids where the given key phrase appears.
+    // key phrase can have one or two words in English alphabetic characters.
+    // return an empty list if search() finds no match in all documents.
+    public List<Integer> search(String keyPhrase) {
+        // homework
+        return new ArrayList<>(); // place holder
+    }
+}
